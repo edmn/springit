8df2ceba2f2670876ca60acba23534e42b5777af
EdDev <eddymartin@gmail.com>
2019-03-20 22:18:08 +0000

    Created entities and refectored using Lombok

--- src/main/java/com/ed/springit/SpringitApplication.java
+++ src/main/java/com/ed/springit/SpringitApplication.java
@@ -1,13 +1,31 @@
 package com.ed.springit;
 
+import com.ed.springit.config.SpringitProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			log.error("CommandLineRunner.run();");
+			log.warn("CommandLineRunner.run();");
+			log.info("CommandLineRunner.run();");
+		};
 	}
 
 }
---
+++ src/main/java/com/ed/springit/config/SpringitProperties.java
@@ -0,0 +1,23 @@
+package com.ed.springit.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("springit")
+public class SpringitProperties {
+
+    /**
+     * This is our welcome message
+     */
+    private String welcomeMsg = "Hello, World!";
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+
+
+
+}
