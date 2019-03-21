7dbc294bac0863537a29130a49311265ee2b2b5d
EdDev <eddymartin@gmail.com>
2019-03-21 22:03:03 +0000

    Initial Repos for link and comment

---
+++ src/main/java/com/ed/springit/repository/LinkRepository.java
@@ -0,0 +1,9 @@
+package com.ed.springit.repository;
+
+import com.ed.springit.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+
+
+}
